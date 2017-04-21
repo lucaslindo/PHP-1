4f74f3cb8f652d5e7cc484ec60f0e30a04ef5b90
MateuSilveira <mateusilveira30@gmail.com>
2017-04-21 20:34:07 -0300

    Create exemplo 1.php

---
+++ exemplo 1.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<meta charset="utf-8" />
+
+</head>
+<body>
+<?php
+$idade=16;
+$nome ="Mateus";
+echo "OlÃ¡ meu nome ",$nome,"<br>Minha idade Ã© ",$idade;
+?>
+
+
+
+</body>
+
+
+</html>
